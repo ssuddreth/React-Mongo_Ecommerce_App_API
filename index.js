c80efe73f5b8fb39dc9236a0defc9f42a92e5ddd
Steven Suddreth <stevensuddreth@gmail.com>
2022-11-12 14:54:49 -0500

    implement jwt and complete auth, cart, order, product, and user routes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const cors = require('cors');
 
 dotenv.config();
@@ -22,6 +25,9 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server is listening on port ${port}`);

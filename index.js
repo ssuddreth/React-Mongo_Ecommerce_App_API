32f31e9d88ef0618bc805a5b77b31973a8b8ccf0
Steven Suddreth <stevensuddreth@gmail.com>
2022-11-14 19:07:44 -0500

    set up initial test implementation of Stripe js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 const cors = require('cors');
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`server is listening on port ${port}`);
